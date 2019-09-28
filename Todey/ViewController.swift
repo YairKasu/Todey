238f417bfc540b50383abcfc529f86e3111c94dc
Yair <kasuker93@gmail.com>
2019-09-28 19:44:21 +0300

    Initial Commit

---
+++ Todey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todey
+//
+//  Created by Yair Kasuker on 28/09/2019.
+//  Copyright © 2019 Yair Kasuker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
